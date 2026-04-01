7fb1f5f0f7b0da75dbe3a17bfedee6299399a5bf
Joao Pedro <95698131+Iohanan-Cephas@users.noreply.github.com>
2026-04-01 18:38:25 -0300

    fix: lint errors

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -1,20 +1,19 @@
-import { Request, Response, NextFunction } from 'express';
-import { z } from 'zod';
+import type { Request, Response, NextFunction } from 'express';
+import type { z } from 'zod';
 
 export const validateData = (schema: z.ZodSchema) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    
     const result = schema.safeParse(req.body);
 
     if (!result.success) {
-      const errorDetails = result.error.issues.map(err => ({
+      const errorDetails = result.error.issues.map((err) => ({
         field: err.path.join('.'),
         message: err.message
       }));
 
       return res.status(400).json({
         error: true,
-        message: "Invalid input data.",
+        message: 'Invalid input data.',
         fields: errorDetails
       });
     }
