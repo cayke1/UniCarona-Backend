bd845b4803cae6bf3b88df0ade75cbc172358ed2
Cayke Daniel <caykedanielcd@gmail.com>
2026-04-22 19:06:50 -0300

    chore: remove unused import

--- src/routes/ride-request.routes.ts
+++ src/routes/ride-request.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middlewares/auth.middleware';
-import { requireRole } from '../middlewares/role.middleware';
 import { validateData } from '../middlewares/validate.middleware';
 import { rideRequestController } from '../controllers/ride-request.controller';
 import { updateRideRequestStatusSchema } from '../schemas/ride-request.schema';
